07f1dc27ed03eb0582e591c909bf146a37bb8c14
kalrabhumikabits <140315416+kalrabhumikabits@users.noreply.github.com>
2024-04-20 23:27:24 +0530

    Update index.js updated comments

--- backend/src/v1/routes/index.js
+++ backend/src/v1/routes/index.js
@@ -6,7 +6,7 @@ const homeController = require("../../controllers/linguistController")
 router.use("/",homeController)
 router.use("/test/:id",)
 
-router.post("/users",homeController.users()) // create new user
+router.post("/users",homeController.users()) // create new user from the app
 router.post("/user/:id",homeController.getUser()) // post user progress
 router.get("/content",homeController.getAllContent()) // get the content
 router.get("/users/:id",homeController.getUserContent()) // fetch user progress and displayed content object
