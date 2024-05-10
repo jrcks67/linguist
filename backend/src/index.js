572388563b661e6cfed9221ea9fb4c3b8cac9636
kalrabhumikabits <2023mt93431@wilp.bits-pilani.ac.in>
2024-05-10 22:08:47 +0530

    fix: adding new backend code
    fix: adding new backend code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 const express = require("express")
+const cors=require("cors");
 const app = express()
 const PORT = 3001
 const v1Routes = require("./v1/routes/index.js")
 app.use(express.json())
 app.use("/",v1Routes)
 app.listen(PORT,()=>{console.log(`Server is running on PORT ${PORT}..`)})
+app.use(cors())
